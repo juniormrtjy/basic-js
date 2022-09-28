0391f931c5f9b1608ceb47e470d07c291e3f043e
Paulo Roberto <paulorobertomrtjy@gmail.com>
2022-09-27 22:43:38 -0300

    books dark mode

--- js/projeto-livros.js
+++ js/projeto-livros.js
@@ -140,3 +140,27 @@ function showOffer() {
     descount.innerHTML = 'Seu valor ainda está abaixo de R$50,00'
   }
 }
+
+// DARK MODE
+
+let darkMode = localStorage.getItem('darkm')
+const mode = document.querySelector('.mode')
+mode.addEventListener('click', () => {
+  if (darkMode != 'on') {
+    document.body.classList.add('dark')
+    localStorage.setItem('darkm', 'on')
+    darkMode = 'on'
+  } else {
+    document.body.classList.remove('dark')
+    localStorage.setItem('darkm', 'off')
+    darkMode = 'off'
+  }
+})
+
+function loadMode() {
+  if (darkMode === 'on') {
+    document.body.classList.add('dark')
+  } else {
+    document.body.classList.remove('dark')
+  }
+}
