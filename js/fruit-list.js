2ba20a7c3e56c3aacb9a182baff0be9860c622be
Paulo Roberto <paulorobertomrtjy@gmail.com>
2022-10-01 17:42:13 -0300

    asd

--- js/fruit-list.js
+++ js/fruit-list.js
@@ -4,5 +4,6 @@ function addItem() {
   fruitArray[0] = fruitArray[0].toUpperCase()
   fruitArray = fruitArray.join('')
   const sectionHtmlAdd = `<section> <div class="wrapper"> <div class="box"><p><span> ${fruitArray} </span></p> <div class="btns"><button>+</button> <button>-</button></div> </div> </div></section>`
-  document.body.innerHTML += sectionHtmlAdd
+  const main = document.querySelector('main')
+  main.innerHTML += sectionHtmlAdd
 }
