a2bd638b0497a67d19bd65dae257682b9ecc5aa3
Paulo Roberto <paulorobertomrtjy@gmail.com>
2022-10-03 20:05:20 -0300

    fix login bug

--- js/light.js
+++ js/light.js
@@ -1,6 +1,5 @@
 const setUsername = document.querySelector('.set-user')
 const getLogin = document.querySelector('.login')
-let login = false
 
 function setUser() {
   if (
@@ -9,7 +8,7 @@ function setUser() {
   ) {
     localStorage.setItem('Username', prompt('Escolha o seu nome de usuário'))
     if (localStorage.getItem('Username') != 'null') {
-      login = true
+      localStorage.setItem('UserIsTrue', true)
     }
   } else {
     alert('Você já escolheu o seu nome')
@@ -17,7 +16,7 @@ function setUser() {
 }
 
 function userLogin() {
-  if (login == true) {
+  if (localStorage.getItem('UserIsTrue') == 'true') {
     alert('Login Efetuado')
   } else {
     alert('Você ainda não registrou um nome de usuário')
