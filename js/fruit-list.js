621d6783b1fe2b2bac8d87147538fa95531879e0
Paulo Roberto <paulorobertomrtjy@gmail.com>
2022-11-21 14:18:59 -0300

    up

--- js/fruit-list.js
+++ js/fruit-list.js
@@ -1,23 +1,39 @@
+// criar section
+function createSection(content) {
+  return `<section><div class="wrapper">${content}</div></section>`
+}
+
+function createDivBox(...content) {
+  return `<div class="box">${content}</div`
+}
+
+function createParagraph(content) {
+  return `<p>${content}</p>`
+}
+
+function createSpan(content) {
+  return `<span>${content}</span>`
+}
+
+function createDivBtn(...content) {
+  return `<div class="btns">${content}</div>`
+}
+
+function createButton(content) {
+  return `<button>${content}</button>`
+}
+
 function addItem() {
   let fruitName = prompt('Digite o nome de alguma fruta')
   let fruitArray = fruitName.split('')
   fruitArray[0] = fruitArray[0].toUpperCase()
   fruitArray = fruitArray.join('')
-  const sectionHtmlAdd = `
-  <section> 
-    <div class="wrapper"> 
-      <div class="box">
-        <p>
-          <span> ${fruitArray} </span>
-        </p> 
-
-        <div class="btns">
-          <button>+</button> 
-          <button>-</button>
-        </div> 
-      </div> 
-    </div>
-  </section>`
+  const sectionHtmlAdd = createSection(
+    createDivBox(
+      createParagraph(createSpan(`${fruitArray}`)) +
+        createDivBtn(createButton('+') + createButton('-'))
+    )
+  )
   const main = document.querySelector('main')
   main.innerHTML += sectionHtmlAdd
 }
