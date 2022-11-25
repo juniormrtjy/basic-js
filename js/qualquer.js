37509d80436605384f95325d6ec45e9c2b0611b1
Paulo Roberto <paulorobertomrtjy@gmail.com>
2022-11-24 22:46:26 -0300

    em andamento

--- js/qualquer.js
+++ js/qualquer.js
@@ -1,10 +1,28 @@
 // register
 const userName = document.querySelector('#username')
 const userPass = document.querySelector('#userpass')
+const registerBtn = document.querySelector('.register')
 
 function register() {
-  localStorage.setItem('Registerusername', userName.value)
-  localStorage.setItem('Registeruserpass', userPass.value)
+  if (
+    localStorage.getItem('Registerusername') == 'null' ||
+    localStorage.getItem('Registerusername') == undefined ||
+    localStorage.getItem('Registerusername') == ''
+  ) {
+    localStorage.setItem('Registerusername', userName.value)
+    localStorage.setItem('Registeruserpass', userPass.value)
+  } else {
+    alert('Você não escolheu um nome de usuário.')
+  }
 }
 const getUserName = localStorage.getItem('Registerusername')
 const getUserPass = localStorage.getItem('Registeruserpass')
+
+if (
+  localStorage.getItem('Registerusername') != 'null' &&
+  localStorage.getItem('Registerusername') != ''
+) {
+  let setLink = 'Registrar'
+  let linkUrl = 'https://www.google.com'
+  registerBtn.innerHTML = setLink.link(linkUrl)
+}
