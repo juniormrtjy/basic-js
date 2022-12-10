1041f4bc452543afac091633ab8e63a6f68beeab
Paulo Roberto <paulorobertomrtjy@gmail.com>
2022-12-09 22:43:47 -0300

    up

--- js/new.js
+++ js/new.js
@@ -1,2 +1,4 @@
 document.body.innerHTML += `<h1> This is my comeback, i think</h1>`
 document.body.innerHTML += `<p>How many exercise i can do for my recap? And, how i can do this while i practice my english. I have a many levels of english. Writting = basic i think. Speak = basic i think. Listen = intermediary. If have more, i dont remember now.</p>`
+
+document.body.innerHTML += `<h2> Just do it</h2>`
