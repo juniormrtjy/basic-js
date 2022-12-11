1b9a6612eda69f4dfdacd7950bd4bcc6bc6bfcab
Paulo Roberto <paulorobertomrtjy@gmail.com>
2022-12-11 17:49:08 -0300

    up

--- js/new.js
+++ js/new.js
@@ -1,8 +1,3 @@
-document.body.innerHTML += `<h1> This is my comeback, i think</h1>`
-document.body.innerHTML += `<p>How many exercise i can do for my recap? And, how i can do this while i practice my english. I have a many levels of english. Writting = basic i think. Speak = basic i think. Listen = intermediary. If have more, i dont remember now.</p>`
-
-document.body.innerHTML += `<h2> Just do it</h2>`
-
 const divRed = document.querySelector('.red')
 const divBlue = document.querySelector('.blue')
 const divGreen = document.querySelector('.green')
