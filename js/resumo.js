6f3257b35a5c13feaca1bbc5a57c38b7c55f4ccc
Paulo Roberto <paulorobertomrtjy@gmail.com>
2023-01-06 10:45:08 -0300

    big changes and the final file about all

---
+++ js/resumo.js
@@ -0,0 +1,25 @@
+/* pensar em algo com tudo o que eu aprendi até aqui
+
+atribuições
+operadores lógicos
+operadores comparativos
+strings
+if
+operador ternário
+switch
+for e while
+arrays
+objetos: propriedades, métodos, construtores
+datas
+const let e var
+manipulação de dados (rocketseat)
+push() e unshift()
+pop() e shift()
+slice()
+splice()
+includes()
+indexOf()
+
+não lembro muito bem de tudo, mas dá p fazer algo
+
+*/
